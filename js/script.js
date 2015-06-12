7b02c9dd610c7784c95aa44e94eb9988cacde76e
Nazarett <gnazarett@gmail.com>
2015-06-11 21:21:09 -0400

    Width

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert("suck my dick")
+//What is the area of a rectangle given
+// that half of the width is 17.5 sq ft
+// and the height is double of its true value which is 84 sq ft
+
+//A = W * H This is the formula to calculate the area.
+//
+
+var width = 17.5 * 2 //This will give us the actual width.
+console.log(width)//Width
+
+
